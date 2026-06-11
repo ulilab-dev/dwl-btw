ac4d06e25fa71342d813687b9c4a230bc489d1ad
ulilab-dev <ullia5733@gmail.com>
2026-06-11 19:04:21 +0700

    dwl

---
+++ dwl-run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export MOZ_ENABLE_WAYLAND=1
+export XDG_CURRENT_DESKTOP=wlroots
+export XDG_MENU_PREFIX="arch-"
+
+export QT_QPA_PLATFORMTHEME="qt6ct"
+export QT_QPA_PLATFORM="wayland;xcb"
+
+(
+sleep 0.5
+wlr-randr --output HDMI-A-1 --mode 1920x1080@100
+waybar
+) &
+
+ waypaper --restore &
+ dunst &
+
+exec dbus-run-session dwl
+
