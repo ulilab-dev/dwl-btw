0b488bfd4ce6ffcc6f513bed9ff18cfadb075f96
ulil <ullia5733@gmail.com>
2026-07-08 16:22:20 +0700

    remove void

--- void/dwl/screenshots.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-X_FILE="$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png" && grim -g "$(slurp)" "$X_FILE" && notify-send -a "Hyprshot" -i "$X_FILE" "Screenshot Saved" "Saved to $(basename "$X_FILE")"
--- void/rofi/clipboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cliphist list | rofi -dmenu -config "~/.config/rofi/clip.rasi" -p " " -display-columns 2 | cliphist decode | wl-copy
