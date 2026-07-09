ae30bab12bf2f087f2cb69eb8a64dc02df75ef8d
ulil <ullia5733@gmail.com>
2026-07-09 12:53:47 +0700

    update

--- arch/dwl/autostart.sh
+++ arch/dwl/autostart.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-wlr-randr --output HDMI-A-1 --mode 1920x1080@100 &
+wlr-randr --output HDMI-A-1 --mode 1920x1080@100 >/dev/null 2>&1 &
 
-waybar &
-waypaper --restore &
-dunst &
+waybar >/dev/null 2>&1 &
+waypaper --restore >/dev/null 2>&1 &
+dunst >/dev/null 2>&1 &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
 
 # clipboard
-wl-paste --type text --watch cliphist store &
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
